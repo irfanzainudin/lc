47afa8368304b80096409c0573b27197ba95b023
irfanzainudin <m.irfan.zain@gmail.com>
2024-03-10 23:34:25 +1100

    finally got it working (30 minutes more or less)

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -1,10 +1,25 @@
-class Solution:
-    def duplicateZeros(self, arr: List[int]) -> None:
-        """
-        Do not return anything, modify arr in-place instead.
-        """
-        for i in arr:
-            if i == 0:
-                print(i)
-            else:
-                print("not zero")
+from typing import List
+
+
+def duplicateZeros(arr: List[int]) -> None:
+    """
+    Do not return anything, modify arr in-place instead.
+    """
+    i = 0
+    while i < len(arr) - 1:
+        if arr[i] == 0:
+            # shift everything to the right
+            j = len(arr) - 1
+            while j > i+1:
+                arr[j] = arr[j-1]
+                j -= 1
+            # set duplicate
+            if i+1 < len(arr):
+                arr[i+1] = 0
+            i += 1
+        i += 1
+    print(arr)
+
+
+arr = [1, 2, 3]
+duplicateZeros(arr)
