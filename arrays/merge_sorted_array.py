fcf8010d065116cf192c99e9046702a9f1a60519
irfanzainudin <m.irfan.zain@gmail.com>
2024-03-15 21:35:58 +1100

    got a correct implementation working, but doesn't seem to work on LeetCode

--- arrays/merge_sorted_array.py
+++ arrays/merge_sorted_array.py
@@ -8,20 +8,33 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     i = 0
     j = 0
     while i < m:
-        if nums1[i] > nums2[i]:
-            shift_elems_to_right(nums1, i)
-            nums1[i] = nums2[i]
-        elif nums1[i] < nums2[i]:
-            pass
+        if nums1[i] == nums2[j]:
+            j += 1
+        elif nums1[i] <= nums2[j]:
+            # shift_elems_to_right(nums1, i+1)
+            # nums1[i+1] = nums2[j]
+            i += 1
         else:
             shift_elems_to_right(nums1, i)
-            nums1[i] = nums2[i]
-        i += 1
+            nums1[i] = nums2[j]
+            i += 1
+    print(nums1)
+
+
+def merge1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    j = 0
+    for i, el in enumerate(nums1):
+        if el == 0:
+            nums1[i] = nums2[j]
+            j += 1
+    nums1 = sorted(nums1)
     print(nums1)
 
 
 def shift_elems_to_right(arr: List[int], idx: int) -> None:
-    # shift everything to the right
     j = len(arr) - 1
     while j > idx+1:
         arr[j] = arr[j-1]
