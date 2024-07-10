fde2dcb17e9e66728f1a91fe1223d8557d1c96f3
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-07-10 21:24:49 +0800

    done 14

--- no14.py
+++ no14.py
@@ -1,34 +1,48 @@
 # Longest Common Prefix
 # wrong
 # wrong
+# looked at solutions
 
 from typing import List
 
 
-def longestCommonPrefix(strs: List[str]) -> str:
-    if len(strs) == 1:
-        return strs[0]
+# def longestCommonPrefix(strs: List[str]) -> str:
+def longestCommonPrefix(v: List[str]) -> str:
+    # if len(strs) == 1:
+    #     return strs[0]
 
-    stri = strs[0]
-    shortest_string_length = len(stri)
-    for string in strs[1:]:
-        if len(string) <= shortest_string_length:
-            shortest_string_length = len(string)
+    # stri = strs[0]
+    # shortest_string_length = len(stri)
+    # for string in strs[1:]:
+    #     if len(string) <= shortest_string_length:
+    #         shortest_string_length = len(string)
 
-    commonPrefix = ''
-    for string in strs[1:]:
-        i = 0
-        while i < shortest_string_length:
-            if stri[i] != string[i]:
-                commonPrefix = commonPrefix[:i]
-                break
-            else:
-                commonPrefix += stri[i]
-            i += 1
+    # commonPrefix = ''
+    # for string in strs[1:]:
+    #     i = 0
+    #     while i < shortest_string_length:
+    #         if stri[i] != string[i]:
+    #             commonPrefix = commonPrefix[:i]
+    #             break
+    #         else:
+    #             commonPrefix += stri[i]
+    #         i += 1
 
-    return commonPrefix
+    # return commonPrefix
+
+    # Looked at solution:
+    ans=""
+    v=sorted(v)
+    first=v[0]
+    last=v[-1]
+    for i in range(min(len(first),len(last))):
+        if(first[i]!=last[i]):
+            return ans
+        ans+=first[i]
+    return ans 
 
 
 print(longestCommonPrefix(['flower', 'flow', 'flight']))
+print(longestCommonPrefix(['flower', 'flower', 'flower']))
 print(longestCommonPrefix(["dog", "racecar", "car"]))
 print(longestCommonPrefix(["a"]))
